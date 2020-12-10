73bfd745d3a60a23f0dca29a28efb8ed0efadbb1
Crono03 <cirelliluigi5@gmail.com>
2020-12-10 12:56:01 +0100

    Le caratteristiche ma le caratteristiche non centrano un fallo con i ruo

--- FootballManager2k21.java
+++ FootballManager2k21.java
@@ -1,6 +1,16 @@
 import java.io.*;
 public class FootballManager2k21{
 
+  public static void definisciRuoli(String Giocatori[]){
+    final int nPortieri=15;
+    final int nDifensori=30;
+    final int nCentrocampisti=35;
+    final int nAttaccanti=20;
+
+    
+
+  }
+
   private static void creaGiocator1(String Giocatori[]){
     String[]Nomi={"Lukabboloy","Sean","TonyFantino","Guesus","AlexObesoNano","Deiso","Carolo","Marvin","EvilKann","Carl","Bernardo","Joele","Gabe","William","Marco","'Ntuoni","Juanni","Sarpeni","Saro","Gregorio","Mariello","Salvatore","Sandro","Frode","Gianni","Moe","Dark","Michele","Pakalu","Pinzkeberg"};
     String[]Cognomi={"Picone","Figliodijuanni","Canadesi","Girelli","Piconetu","Chamberlain","Kane","Cosimo","Russell","Jordano","De Magistris","Cosmo","Froicio","Berlinguer","Boschetti","Dula","Scavoli","Bomba","Settebello","Laureantont","Battilamiera","Rollins","Egregio","Mazza","Pinorat","Hamilton","Gang","Gianni","Misseri","Andreotti","Papito","Osas","Maggi","Fonino"};
@@ -20,7 +30,7 @@ public class FootballManager2k21{
       for (int j=0 ;j<i ;j++ ) {
         String gae = Giocatori[j];
 
-        if ( (ciocatori.equals(gae) )  ) {
+        if ( (ciocatori.equals(gae) )  ) {  //gae=GenerazioneArrayEnonsapevochemetteredopo
           controlloNomi=true ;
         }
       }
@@ -43,7 +53,11 @@ public class FootballManager2k21{
     br = new BufferedReader(isr);
 
     String[] Giocatori=new String[100];
+    Int[] Caratteristiche1=new Int[100]; //velocita,tuffo
+    Int[] Caratteristiche2=new Int[100]; //potenza,contrasto,posizione
+    Int[] Caratteristiche3=new Int[100]; //dinamicità,visione di gioco, riflessi, scivolata
     creaGiocator1(Giocatori);
+
   }
 
 }
