d958f5e503c438350e4360912890578e148134ed
picomuro <picone.luca777@gmai.com>
2020-12-10 21:04:25 +0100

    v 0.71

--- FootballManager2k21.java
+++ FootballManager2k21.java
@@ -53,8 +53,11 @@ public class FootballManager2k21{
 
     public static void clearScreen() {
 
-      System.out.print("\033[H\033[2J");
-      System.out.flush();
+      try{
+
+        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+        
+      }catch(Exception e){System.out.println(e);}
 
     }
 
